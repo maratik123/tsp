87163325f5d35fa31b0c4fe4e27181a333b84f9d
Marat Bukharov <maratik@yandex-team.ru>
2024-06-02 15:10:01 +0300

    cleanup

--- src/distance.rs
+++ src/distance.rs
@@ -1,51 +1,8 @@
-use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::marker::PhantomData;
 
-use crate::model::{Airport, AirportIdx};
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct Distances<'a> {
-    dists: HashMap<&'a str, HashMap<&'a str, f64>>,
-}
-
-impl<'a, 'b> Distances<'b> {
-    pub fn between(&self, apt1: &'a str, apt2: &'a str) -> Result<f64, KeyNotFound<'a>> {
-        let (apt1, apt2) = if apt1 > apt2 {
-            (apt1, apt2)
-        } else {
-            (apt2, apt1)
-        };
-        let sub_map = self.dists.get(apt1).ok_or(KeyNotFound(apt1))?;
-        if apt1 == apt2 {
-            Ok(0.0)
-        } else {
-            sub_map.get(apt2).copied().ok_or(KeyNotFound(apt2))
-        }
-    }
-}
-
-impl<'a> From<&'a [Airport]> for Distances<'a> {
-    fn from(value: &'a [Airport]) -> Self {
-        let mut value: Vec<_> = value.iter().collect();
-        value.sort_unstable_by_key(|a| a.icao.as_str());
-        let dists = value
-            .iter()
-            .enumerate()
-            .map(|(apt1_i, apt1)| {
-                (
-                    apt1.icao.as_str(),
-                    value[..apt1_i]
-                        .iter()
-                        .map(|apt2| (apt2.icao.as_str(), apt1.distance_to(apt2)))
-                        .collect::<HashMap<_, _>>(),
-                )
-            })
-            .collect();
-        Distances { dists }
-    }
-}
+use crate::model::AirportIdx;
 
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
 pub struct DistancesIdx<'a> {
@@ -103,6 +60,7 @@ mod tests {
     use std::f64::consts::FRAC_PI_2;
 
     use crate::math::great_circle;
+    use crate::model::Airport;
     use crate::types::field::coord::{
         Coord, Latitude, LatitudeHemisphere, Longitude, LongitudeHemisphere,
     };
@@ -178,23 +136,6 @@ mod tests {
     }
 
     #[test]
-    fn between_test() {
-        let airports = airports_template();
-        let distances = Distances::from(&airports[..]);
-        let quarter = quarter();
-        for apt1 in ["A", "B", "C"] {
-            for apt2 in ["A", "B", "C"] {
-                assert_eq!(
-                    distances.between(apt1, apt2),
-                    Ok(if apt1 == apt2 { 0.0 } else { quarter })
-                )
-            }
-            assert_eq!(distances.between("D", apt1), Err(KeyNotFound("D")));
-            assert_eq!(distances.between(apt1, "D"), Err(KeyNotFound("D")));
-        }
-    }
-
-    #[test]
     fn idx_between_test() {
         let airports = airports_template();
         let apt_idx = AirportIdx::new(&airports).unwrap();
@@ -210,23 +151,6 @@ mod tests {
             assert_eq!(distances_idx.between(3, apt1), None);
             assert_eq!(distances_idx.between(apt1, 3), None);
         }
-    }
-
-    #[test]
-    fn test_distances() {
-        let airports = airports_template();
-        let distances = Distances::from(&airports[..]);
-        let quarter = quarter();
-        assert_eq!(
-            distances,
-            Distances {
-                dists: HashMap::from([
-                    ("A", HashMap::new()),
-                    ("B", HashMap::from([("A", quarter)])),
-                    ("C", HashMap::from([("A", quarter), ("B", quarter)]))
-                ])
-            }
-        );
     }
 
     #[test]
